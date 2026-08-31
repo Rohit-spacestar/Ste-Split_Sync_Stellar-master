454d878d06f6dac7e8ccbd18a4cd4e26da07c770
Ravi <spacestarj2@gmail.com>
2026-08-31 18:16:33 +0530

    Update next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   async headers() {
     return [
-      {
+      { 
         source: "/(.*)",
         headers: [
           {
